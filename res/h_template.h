2bf97b75c9cd46ae8b3a7057bfd45dc52d691d59
Edwin Hayes <edwin.hayes@pg.canterbury.ac.nz>
2011-12-04 15:09:09 +1300

    Heavily refactored and modified bld/VFstart to improve user interface and maintainability.
    Created bld/create/create_files script to allow the addition of further template generated files to an existing component.
    
    Fixed some issues with the preprocessing bits.
    
    
    Former-commit-id: 679b78a0f63321797a0af391ec4c68319adda93c
    Former-commit-id: 47fda72279b8d86e90a090f84bdef70b5653c752

--- res/h_template.h
+++ res/h_template.h
@@ -2,7 +2,7 @@
  *
  *  FILE: 		<<<TC_INSERTS_FILE_NAME_HERE>>>
  *
- *  PROJECT ASPECT:	<<<TC_INSERTS_ASPECT_HERE>>>
+ *  SUB-SYSTEM:		<<<TC_INSERTS_SUBSYSTEM_HERE>>>
  *
  *  COMPONENT:		<<<TC_INSERTS_COMPONENT_NAME_HERE>>>
  *
@@ -14,18 +14,18 @@
  *
  *  DATE CREATED:	<<<TC_INSERTS_DATE_HERE>>>
  *
- *	This is the header file which matches...
+ *	This is the header file which matches <<<TC_INSERTS_C_FILE_NAME_HERE>>>...
  * 
  ********************************************************************************************************************************/
 
 // Only include this header file once.
-#ifndef __<<<TC_INSERTS_FILE_BASENAME_HERE>>>_H__
-#define __<<<TC_INSERTS_FILE_BASENAME_HERE>>>_H__
+#ifndef __<<<TC_INSERTS_UC_FILE_BASENAME_HERE>>>_H__
+#define __<<<TC_INSERTS_UC_FILE_BASENAME_HERE>>>_H__
 
 // INCLUDE REQUIRED HEADER FILES.
 
 // Include the required IO header file.
-#include <<<TC_INSERTS_IO_FILE_NAME_HERE>>>
+#include >>>TC_INSERTS_IO_FILE_NAME_HERE<<<
 
 // DEFINE PUBLIC TYPES AND ENUMERATIONS.
 
@@ -35,6 +35,6 @@
 
 int main(void);
 
-#endif /*__<<<TC_INSERTS_FILE_BASENAME_HERE>>>_H__*/
+#endif /*__<<<TC_INSERTS_UC_FILE_BASENAME_HERE>>>_H__*/
 
 // ALL DONE.
