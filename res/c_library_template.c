9899e69370282eff943041ffdac90a57869ce419
Edwin Hayes <edwin.hayes@pg.canterbury.ac.nz>
2011-12-09 11:47:18 +1300

    Added support for creating 'libraries' which are common across multiple build configurations.
    Former-commit-id: 0e199c7ffd254b698d299f593f65a734d6d20b62
    Former-commit-id: 51e7a37aedbd24bf71ce0bd7ad7bd70b93e13d9c

---
+++ res/c_library_template.c
@@ -0,0 +1,31 @@
+/********************************************************************************************************************************
+ *
+ *  FILE: 		<<<TC_INSERTS_FILE_NAME_HERE>>>
+ *
+ *  LIBRARY:		<<<TC_INSERTS_LIBRARY_NAME_HERE>>>
+ *
+ *  BCONFS:		<<<TC_INSERTS_BUILD_LIST_HERE>>>
+ *
+ *  AUTHOR: 		<<<TC_INSERTS_AUTHOR_NAME_HERE>>>
+ *
+ *  DATE CREATED:	<<<TC_INSERTS_DATE_HERE>>>
+ *
+ *	Description goes here...
+ *
+ ********************************************************************************************************************************/
+
+// INCLUDE THE MATCHING HEADER FILE.
+
+#include ">>>TC_INSERTS_H_FILE_NAME_HERE<<<"
+
+// DEFINE PRIVATE TYPES AND STRUCTS.
+
+// DECLARE PRIVATE GLOBAL VARIABLES.
+
+// DEFINE PRIVATE FUNCTION PROTOTYPES.
+
+// IMPLEMENT PUBLIC FUNCTIONS.
+
+// IMPLEMENT PRIVATE FUNCTIONS.
+
+// ALL DONE.
---
+++ res/h_library_template.h
@@ -0,0 +1,31 @@
+/********************************************************************************************************************************
+ *
+ *  FILE: 		<<<TC_INSERTS_FILE_NAME_HERE>>>
+ *
+ *  LIBRARY:		<<<TC_INSERTS_LIBRARY_NAME_HERE>>>
+ *
+ *  BCONFS:		<<<TC_INSERTS_BUILD_LIST_HERE>>>
+ *
+ *  AUTHOR: 		<<<TC_INSERTS_AUTHOR_NAME_HERE>>>
+ *
+ *  DATE CREATED:	<<<TC_INSERTS_DATE_HERE>>>
+ *
+ *	This is the header file which matches <<<TC_INSERTS_C_FILE_NAME_HERE>>>...
+ * 
+ ********************************************************************************************************************************/
+
+// Only include this header file once.
+#ifndef __<<<TC_INSERTS_UC_FILE_BASENAME_HERE>>>_H__
+#define __<<<TC_INSERTS_UC_FILE_BASENAME_HERE>>>_H__
+
+// INCLUDE REQUIRED HEADER FILES.
+
+// DEFINE PUBLIC TYPES AND ENUMERATIONS.
+
+// DECLARE PUBLIC GLOBAL VARIABLES.
+
+// DEFINE PUBLIC FUNCTION PROTOTYPES.
+
+#endif /*__<<<TC_INSERTS_UC_FILE_BASENAME_HERE>>>_H__*/
+
+// ALL DONE.
