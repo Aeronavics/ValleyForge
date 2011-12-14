c1094dd16cdf55b32e24f0a08742d14a7c79b3cb
Paul Bowler <phb33@uclive.ac.nz>
2011-12-15 11:11:41 +1300

    Updated mem.h to suit implementation mem.c for ATmega2560 target. Edited to remove options to read/write/copy to/from flash. Also removed unwarranted references to fuse-bits.
    Former-commit-id: 391027e347e35a9dd85249845259b7f58ef379cb
    Former-commit-id: e38a451b48fdd78d0043fd591f870ac459011cc1

--- res/common/hal/mem.h
+++ res/common/hal/mem.h
@@ -12,6 +12,8 @@
  *
  *	This is the header file which matches mem.c. Provides functionality for writing and reading to EEPROM memory.
  * 
+ *  Revised 14-12-2011 by Paul Bowler to remove options to read/write/copy to/from flash. Also removed unwarranted
+ *  references to fuse-bits.
  ********************************************************************************************************************************/
 
 // Only include this header file once.
@@ -29,53 +31,56 @@
 // Include the common HAL stuff.
 #include "hal.h"
 
-/**
-		 * Write to a memory address
-		 * 
-		 *
-		 * @param  dst		32 bit destination address
-		 * @param  dst_type enumeration showing type of memory written to (EEPROM, FLASH, FUSE_BITS)
-		 * @param  data		A pointer to the start of the data to be stored
-		 * @param  length	The number of bytes to be stored
-		 * @return uint8_t 	Result, 0 for success, various other numbers refer to error codes, such as memory out of bounds.
-		 */
-uint8_t writeMem(uint32_t dst, uint8_t dst_type, void* data, uint16_t length);
+// DEFINE PUBLIC TYPES AND ENUMERATIONS.
+
+/*Enumerated list of error returns. 
+* ADDRESS_OUT_OF_BOUNDS : 	the provided source or destination address is out of bounds of EEPROM
+* PROCESS_ERROR :	the copy/read/write/clear process overreaches the limits of EEPROM
+*/
+enum error_returns {ADDRESS_OUT_OF_BOUNDS = 1, PROCESS_ERROR};
 
 /**
-		 * Read from memory address
-		 * 
-		 *
-		 * @param  src		32 bit memory source address
-		 * @param  src_type enumeration showing type of memory read from (EEPROM, FLASH, FUSE_BITS)
-		 * @param  dst		A pointer to the start of the destination where the data will be stored
-		 * @param  length	The number of bytes to be read
-		 * @return uint8_t 	Result, 0 for success, various other numbers refer to error codes, such as memory out of bounds.
-		 */
-uint8_t readMem(uint32_t src, uint8_t src_type, void* dst, uint16_t length);
+ * Write to an EEPROM memory address
+ * 
+ *
+ * @param  dst		32 bit EEPROM destination address
+ * @param  data		A pointer to the start of the data to be stored
+ * @param  length	The number of bytes to be stored
+ * @return uint8_t 	Result, 0 for success, various other numbers refer to error codes, such as memory out of bounds.
+ */
+uint8_t writeMem(uint32_t dst, void* data, uint16_t length);
 
 /**
-		 *Copy from one memory address to another
-		 * 
-		 *
-		 * @param  src		32 bit memory source address
-		 * @param  src_type enumeration showing type of memory read from (EEPROM, FLASH, FUSE_BITS)
-		 * @param  dst		32 bit destination address
-		 * @param  dst_type enumeration showing type of memory written to (EEPROM, FLASH, FUSE_BITS)
-		 * @param  length	The number of bytes to be copied
-		 * @return uint8_t 	Result, 0 for success, various other numbers refer to error codes, such as memory out of bounds.
-		 */		
-uint8_t cpyMem(uint32_t src,uint8_t src_type, uint32_t dst, uint32_t dst_type, uint16_t length);
+ * Read from an EEPROM memory address
+ * 
+ *
+ * @param  src		32 bit EEPROM source address
+ * @param  dst		A pointer to the start of the destination where the data will be stored
+ * @param  length	The number of bytes to be read
+ * @return uint8_t 	Result, 0 for success, various other numbers refer to error codes, such as memory out of bounds.
+ */
+uint8_t readMem(uint32_t src, void* dst, uint16_t length);
 
 /**
-		 * Clear a section of memory (set bits to one)
-		 * 
-		 *
-		 * @param  address	32 bit memory address
-		 * @param  add_type enumeration showing type of memory to be cleared (EEPROM, FLASH, FUSE_BITS)
-		 * @param  length	The number of bytes to be cleared
-		 * @return uint8_t 	Result, 0 for success, various other numbers refer to error codes, such as memory out of bounds.
-		 */		
-uint8_t clearMem(uint32_t address,uint8_t add_type, uint16_t length);
+ * Copy from one EEPROM memory location to another
+ * 
+ *
+ * @param  src		32 bit EEPROM source address
+ * @param  dst		32 bit EEPROM destination address
+ * @param  length	The number of bytes to be copied
+ * @return uint8_t 	Result, 0 for success, various other numbers refer to error codes, such as memory out of bounds.
+ */		
+uint8_t cpyMem(uint32_t src, uint32_t dst, uint16_t length);
+
+/**
+ * Clear a section of EEPROM memory
+ * 
+ *
+ * @param  address	32 bit memory address
+ * @param  length	The number of bytes to be cleared
+ * @return uint8_t 	Result, 0 for success, various other numbers refer to error codes, such as memory out of bounds.
+ */		
+uint8_t clearMem(uint32_t address, uint16_t length);
 
 
 #endif /*__MEM_H__*/
