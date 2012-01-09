b0aa51d9641ba5b9a943629d60bda99cdd0956ec
Edwin Hayes <edwin.hayes@pg.canterbury.ac.nz>
2012-01-09 15:27:14 +1300

    Added support for creating both C and CPP template files.
    Former-commit-id: 345c70e03a0584371ef8e15560527f34b28d540c
    Former-commit-id: b17a8983a1a4a306666462d7c3803bd2b871896f [formerly f382f803e7c8b5b58db2b85d040e744121ce18c8]
    Former-commit-id: f0bf845eb826e8d78d4f27525fc71c7d077b5b7c

---
+++ res/cpp_library_template.cpp
@@ -0,0 +1,39 @@
+/********************************************************************************************************************************
+ *
+ *  FILE: 		<<<TC_INSERTS_FILE_NAME_HERE>>>
+ *
+ *  LIBRARY:		<<<TC_INSERTS_LIBRARY_NAME_HERE>>>
+ *
+ *  BCONFS:		<<<TC_INSERTS_BUILD_LIST_HERE>>>
+ *
+ *  AUTHOR: 		<<<TC_INSERTS_AUTHOR_NAME_HERE>>>
+ *
+ *  DATE CREATED:	<<<TC_INSERTS_DATE_HERE>>>
+ *
+ *	Description goes here...
+ *
+ ********************************************************************************************************************************/
+
+// INCLUDE THE MATCHING HEADER FILE.
+
+#include ">>>TC_INSERTS_H_FILE_NAME_HERE<<<"
+
+// DEFINE PRIVATE MACROS.
+
+// DEFINE PRIVATE TYPES AND STRUCTS.
+
+// DEFINE PRIVATE CLASSES.
+
+// DECLARE PRIVATE GLOBAL VARIABLES.
+
+// DEFINE PRIVATE FUNCTION PROTOTYPES.
+
+// IMPLEMENT PUBLIC STATIC FUNCTIONS.
+
+// IMPLEMENT PUBLIC CLASS FUNCTIONS.
+
+// IMPLEMENT PRIVATE STATIC FUNCTIONS.
+
+// IMPLEMENT PRIVATE CLASS FUNCTIONS.
+
+// ALL DONE.
---
+++ res/cpp_template.cpp
@@ -0,0 +1,49 @@
+/********************************************************************************************************************************
+ *
+ *  FILE: 		<<<TC_INSERTS_FILE_NAME_HERE>>>
+ *
+ *  SUB-SYSTEM:		<<<TC_INSERTS_SUBSYSTEM_HERE>>>
+ *
+ *  COMPONENT:		<<<TC_INSERTS_COMPONENT_NAME_HERE>>>
+ *
+ *  TARGET:		<<<TC_INSERTS_TARGET_HERE>>>
+ *
+ *  PLATFORM:		<<<TC_INSERTS_PLATFORM_HERE>>>
+ *
+ *  AUTHOR: 		<<<TC_INSERTS_AUTHOR_NAME_HERE>>>
+ *
+ *  DATE CREATED:	<<<TC_INSERTS_DATE_HERE>>>
+ *
+ *	Description goes here...
+ *
+ ********************************************************************************************************************************/
+
+// INCLUDE THE MATCHING HEADER FILE.
+
+#include "<<<TC_INSERTS_H_FILE_NAME_HERE>>>"
+
+// DEFINE PRIVATE MACROS.
+
+// DEFINE PRIVATE TYPES AND STRUCTS.
+
+// DEFINE PRIVATE CLASSES.
+
+// DECLARE PRIVATE GLOBAL VARIABLES.
+
+// DEFINE PRIVATE FUNCTION PROTOTYPES.
+
+// IMPLEMENT PUBLIC STATIC FUNCTIONS.
+
+int main(void)
+{
+	// All done.
+	return 0;
+}
+
+// IMPLEMENT PUBLIC CLASS FUNCTIONS.
+
+// IMPLEMENT PRIVATE STATIC FUNCTIONS.
+
+// IMPLEMENT PRIVATE CLASS FUNCTIONS.
+
+// ALL DONE.
