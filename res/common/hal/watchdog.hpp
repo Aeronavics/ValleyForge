8a8afd45b66f0ac621f4ed9e8100a276d4d747ca
Edwin Hayes <edwin.hayes@pg.canterbury.ac.nz>
2012-07-02 20:18:56 +1200

    Added GPL notices to the top of relevant files.
    Former-commit-id: 703d095d42f5fe8ea6d63601631105b66f0267ba
    Former-commit-id: 57288398c4d20a7451a197fdf5b8429f4455827a [formerly 3612d5fcab998447f765f253da82b9c2891a6383]
    Former-commit-id: def4d5d6ee76276ddd79dd7d9fcedc3f599f9945

--- res/common/hal/watchdog.hpp
+++ res/common/hal/watchdog.hpp
@@ -1,3 +1,18 @@
+// Copyright (C) 2011  Unison Networks Ltd
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  *  @addtogroup		hal
  *
@@ -12,9 +27,21 @@
  *  This is the header file which matches watchdog.c.  Allows control over the processor watchdog timer.
  * 
  *  @section Licence
- *  LICENCE GOES HERE
  * 
+ * Copyright (C) 2011  Unison Networks Ltd
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
  *  @section Description
  *  The watchdog timer is a timer that constantly runs in the background. If the watchdog timer is active, then if it reaches its top value (set
