5f1e50997b8e3358a5e6a18d8327612a3f3a3e49
George Wareing <grw83@uclive.ac.nz>
2013-01-29 16:52:45 +1300

    CANSPI bootloader working on atmega64m1 and at90can128. application/bootloader interface incomplete.
    Former-commit-id: 6d057d71c931a86c0ac383e6eab1ca0805de6469
    Former-commit-id: 09af7fd96acc5bdc45a087126eb42de7913dac3e [formerly f0c6928215618e7ed639895aeb270f76265d814d]
    Former-commit-id: b1a4224de6bfe9b0ee1f5fd93791df7436bac5e0

---
+++ res/avr/bootloader/application_interface/application_interface_module_constants_can.hpp
@@ -0,0 +1,16 @@
+/********************************************************************************************************************************
+ *
+ *  FILE: 		shared_bootloader_module_constants_can.hpp
+ *
+ *  AUTHOR: 		George
+ *
+ *  DATE CREATED:	24-1-2012
+ *
+ *	Header file that contains the struct of information that can be accessed from the bootloader communication module.
+ *
+ ********************************************************************************************************************************/
+ 
+struct Shared_bootloader_module_constants{
+	uint8_t node_id;
+	uint16_t baud_rate;
+};
