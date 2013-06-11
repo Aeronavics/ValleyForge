cc1dd967c67e8c5587e55d06a58c89de8244b43f
Edwin Hayes <edwin.hayes@pg.canterbury.ac.nz>
2013-06-11 16:59:24 +1200

    Added support for the Luminary LM3S1968 (ARM Cortex-M3) target, including uploading code via OpenOCD.
    Former-commit-id: 00e8fdea31065b522be3d1fbf390ae924d7614fd
    Former-commit-id: 462a5aa2ea67620dc5731160dcb41b50a3428c5d [formerly 155fbccacbb7fa3f92d7c1bc95f8c3522c2afa62]
    Former-commit-id: 46420d3d84deb03724b216eafc83e1eb49f66001

---
+++ res/arm/embedded-arm/lm3s1968_startup.h
@@ -0,0 +1,30 @@
+// Copyright (C) 2011  Unison Networks Ltd
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/********************************************************************************************************************************
+ *
+ *  FILE: 		lm3s1968_startup.h
+ *
+ *  AUTHOR: 		Edwin Hayes
+ *
+ *  DATE CREATED:	11-06-2013
+ *
+ *	Dummy header file which indicates to the toolchain that the corresponding C source file will be required.
+ *
+ ********************************************************************************************************************************/
+
+// We don't need to do anything here.
+
+// ALL DONE.
