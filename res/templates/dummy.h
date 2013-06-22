b0f4764a60926ad891bb72548e2a53a64cab3989
Edwin Hayes <edwin.hayes@pg.canterbury.ac.nz>
2013-06-22 14:24:28 +1200

    Added a script to make dummy header files, to assist when using software which doesn't comply with our normal dependency detection requirements.
    Former-commit-id: 50ed36a48f4e5083da57558c58f03c9f230df673
    Former-commit-id: 5e21c46b9a4d257374475d5cf6f62a7211dbd3e9 [formerly b44f300aad0051690ae65dd74acc741579afb9a6]
    Former-commit-id: 7eddbf521a8a836568dbb2e87362e80127820144

---
+++ res/templates/dummy.h
@@ -0,0 +1,14 @@
+/********************************************************************************************************************************
+ *
+ *	DUMMY HEADER FILE.
+ *
+ *	This header file was generated automatically, so that the similarly named source file will be detected for linking.
+ *
+ ********************************************************************************************************************************/
+
+// NOTE - This file was generated automatically.  Do NOT make changes to this file; they may be overwritten without warning.
+
+// We don't need to do anything here.
+
+// ALL DONE.
+
