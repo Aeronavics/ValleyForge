57a7e8844bc6b8e09c231f85f82ae4cf42af3a19
student <gjx10@uclive.ac.nz>
2013-11-21 14:04:52 +1300

    Changed the functions based on the newer simpler implementation which doesn't have a distinct class for CAN tree
    Former-commit-id: 2e44fba6ac200944cb4d2ff2fe8f2e832a8423bc
    Former-commit-id: 4cad01f9d8d2236912b2e5f5fbba2970a6775794 [formerly 817e80c8f200de32daebecb6ed0b8733f8cee875]
    Former-commit-id: fa418ccdef5129b674192cdb12d9d50c35825545

--- res/avr/hal/can.cpp
+++ res/avr/hal/can.cpp
@@ -32,119 +32,171 @@
 // INCLUDE THE MATCHING HEADER FILE.
 
 #include "<<<TC_INSERTS_H_FILE_NAME_HERE>>>"
+#include "can_platform.hpp"
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 /********************************************************************************************************************************/
-Can_object_mode Can_object::get_mode(void)
+
+class Can_imp
 {
+	public:
+		//Methods
+		Can_command_response initialise(Can_rate rate, Can_mode mode);
+		
+		void transmit(Can_object obj, Can_message msg);
+		
+	private:
+		//Methods
+		
+		//Fields
+		
+}
+
+Can_command_response Can_imp::initialise(Can_rate rate, Can_mode mode)
+{
+	#ifndef <<<TC_INSERTS_CLK_SPEED_IN_MHZ_HERE>>>
+		#  error  This CLK_SPEED_IN_MHZ is not set
+	#endif
+
+	//Set global variables depending on required baud rate at the chosen clock speed
+	#if <<<TC_INSERTS_CLK_SPEED_IN_MHZ_HERE>>> == 16000             //!< Fclkio = 16 MHz, Tclkio = 62.5 ns
+		if (rate == CAN_100K)       //!< -- 100Kb/s, 16x Tscl, sampling at 75%
+		{
+			CONF_CANBT1 = 0x12;       // Tscl  = 10x Tclkio = 625 ns
+			CONF_CANBT2 = 0x0C;       // Tsync = 1x Tscl, Tprs = 7x Tscl, Tsjw = 1x Tscl
+			CONF_CANBT3 = 0x37;       // Tpsh1 = 4x Tscl, Tpsh2 = 4x Tscl, 3 sample points
+		}
+	    else if (rate == CAN_125K)  //!< -- 125Kb/s, 16x Tscl, sampling at 75%
+	    {
+			CONF_CANBT1 = 0x0E;       // Tscl  = 8x Tclkio = 500 ns
+			CONF_CANBT2 = 0x0C;       // Tsync = 1x Tscl, Tprs = 7x Tscl, Tsjw = 1x Tscl
+			CONF_CANBT3 = 0x37;       // Tpsh1 = 4x Tscl, Tpsh2 = 4x Tscl, 3 sample points
+		}
+		else if (rate == CAN_200K)  //!< -- 200Kb/s, 16x Tscl, sampling at 75%
+		{
+			CONF_CANBT1 = 0x08;       // Tscl  = 5x Tclkio = 312.5 ns
+	        CONF_CANBT2 = 0x0C;       // Tsync = 1x Tscl, Tprs = 7x Tscl, Tsjw = 1x Tscl
+	        CONF_CANBT3 = 0x37;       // Tpsh1 = 4x Tscl, Tpsh2 = 4x Tscl, 3 sample points
+		}
+		else if (rate == CAN_250K)       //!< -- 250Kb/s, 16x Tscl, sampling at 75%
+		{
+			CONF_CANBT1 = 0x06;       // Tscl  = 4x Tclkio = 250 ns
+			CONF_CANBT2 = 0x0C;       // Tsync = 1x Tscl, Tprs = 7x Tscl, Tsjw = 1x Tscl
+			CONF_CANBT3 = 0x37;       // Tpsh1 = 4x Tscl, Tpsh2 = 4x Tscl, 3 sample points
+		}
+	    else if (rate == CAN_500K)       //!< -- 500Kb/s, 8x Tscl, sampling at 75%
+	    {
+			CONF_CANBT1 = 0x06;       // Tscl = 4x Tclkio = 250 ns
+			CONF_CANBT2 = 0x04;       // Tsync = 1x Tscl, Tprs = 3x Tscl, Tsjw = 1x Tscl
+			CONF_CANBT3 = 0x13;       // Tpsh1 = 2x Tscl, Tpsh2 = 2x Tscl, 3 sample points
+		}
+	    else if (rate == CAN_1000K)      //!< -- 1 Mb/s, 8x Tscl, sampling at 75%
+	    {
+			CONF_CANBT1 = 0x02;       // Tscl  = 2x Tclkio = 125 ns
+			CONF_CANBT2 = 0x04;       // Tsync = 1x Tscl, Tprs = 3x Tscl, Tsjw = 1x Tscl
+			CONF_CANBT3 = 0x13;       // Tpsh1 = 2x Tscl, Tpsh2 = 2x Tscl, 3 sample points
+		}
+
+	#elif <<<TC_INSERTS_CLK_SPEED_IN_MHZ_HERE>>> == 12000           //!< Fclkio = 12 MHz, Tclkio = 83.333 ns
+	    if (rate == CAN_100K)      //!< -- 100Kb/s, 20x Tscl, sampling at 75%
+	    {
+			CONF_CANBT1 = 0x0A;       // Tscl  = 6x Tclkio = 500 ns
+			CONF_CANBT2 = 0x0E;       // Tsync = 1x Tscl, Tprs = 8x Tscl, Tsjw = 1x Tscl
+			CONF_CANBT3 = 0x4B;       // Tpsh1 = 6x Tscl, Tpsh2 = 5x Tscl, 3 sample points
+		}
+	    else if (rate == CAN_125K)      //!< -- 125Kb/s, 16x Tscl, sampling at 75%
+	    {
+			CONF_CANBT1 = 0x0A;       // Tscl  = 6x Tclkio = 500 ns
+	        CONF_CANBT2 = 0x0C;       // Tsync = 1x Tscl, Tprs = 7x Tscl, Tsjw = 1x Tscl
+	        CONF_CANBT3 = 0x37;       // Tpsh1 = 4x Tscl, Tpsh2 = 4x Tscl, 3 sample points
+		}
+	    else if (rate == CAN_200K)       //!< -- 200Kb/s, 20x Tscl, sampling at 75%
+	    {
+			CONF_CANBT1 = 0x04;       // Tscl  = 3x Tclkio = 250 ns
+			CONF_CANBT2 = 0x0E;       // Tsync = 1x Tscl, Tprs = 8x Tscl, Tsjw = 1x Tscl
+			CONF_CANBT3 = 0x4B;       // Tpsh1 = 6x Tscl, Tpsh2 = 5x Tscl, 3 sample points
+		}
+		else if (rate == CAN_250K)      //!< -- 250Kb/s, 16x Tscl, sampling at 75%
+		{
+			CONF_CANBT1 = 0x04;       // Tscl  = 3x Tclkio = 250 ns
+	        CONF_CANBT2 = 0x0C;       // Tsync = 1x Tscl, Tprs = 7x Tscl, Tsjw = 1x Tscl
+			CONF_CANBT3 = 0x37;       // Tpsh1 = 4x Tscl, Tpsh2 = 4x Tscl, 3 sample points
+		}
+	    else if (rate == CAN_500K)       //!< -- 500Kb/s, 12x Tscl, sampling at 75%
+	    {
+	        CONF_CANBT1 = 0x02;       // Tscl  = 2x Tclkio = 166.666 ns
+			CONF_CANBT2 = 0x08;       // Tsync = 1x Tscl, Tprs = 5x Tscl, Tsjw = 1x Tscl
+			CONF_CANBT3 = 0x25;       // Tpsh1 = 3x Tscl, Tpsh2 = 3x Tscl, 3 sample points
+		}
+	    else if (rate == CAN_1000K)      //!< -- 1 Mb/s, 12x Tscl, sampling at 75%
+	    {
+	        CONF_CANBT1 = 0x00;       // Tscl  = 1x Tclkio = 83.333 ns
+	        CONF_CANBT2 = 0x08;       // Tsync = 1x Tscl, Tprs = 5x Tscl, Tsjw = 1x Tscl
+            CONF_CANBT3 = 0x25;       // Tpsh1 = 3x Tscl, Tpsh2 = 3x Tscl, 3 sample points
+		}
+
+	#elif <<<TC_INSERTS_CLK_SPEED_IN_MHZ_HERE>>> == 8000              //!< Fclkio = 8 MHz, Tclkio = 125 ns
+	    if (rate == CAN_100K)       //!< -- 100Kb/s, 16x Tscl, sampling at 75%
+	    {
+	        CONF_CANBT1 = 0x08;       // Tscl  = 5x Tclkio = 625 ns
+	        CONF_CANBT2 = 0x0C;       // Tsync = 1x Tscl, Tprs = 7x Tscl, Tsjw = 1x Tscl
+	        CONF_CANBT3 = 0x37;       // Tpsh1 = 4x Tscl, Tpsh2 = 4x Tscl, 3 sample points
+		}
+	    else if (rate == CAN_125K)       //!< -- 125Kb/s, 16x Tscl, sampling at 75%
+	    {
+	        CONF_CANBT1 = 0x06;       // Tscl  = 4x Tclkio = 500 ns
+	        CONF_CANBT2 = 0x0C;       // Tsync = 1x Tscl, Tprs = 7x Tscl, Tsjw = 1x Tscl
+	        CONF_CANBT3 = 0x37;       // Tpsh1 = 4x Tscl, Tpsh2 = 4x Tscl, 3 sample points
+		}
+	    else if (rate == CAN_200K)       //!< -- 200Kb/s, 20x Tscl, sampling at 75%
+	    {
+	        CONF_CANBT1 = 0x02;       // Tscl  = 2x Tclkio = 250 ns
+	        CONF_CANBT2 = 0x0E;       // Tsync = 1x Tscl, Tprs = 8x Tscl, Tsjw = 1x Tscl
+	        CONF_CANBT3 = 0x4B;       // Tpsh1 = 6x Tscl, Tpsh2 = 5x Tscl, 3 sample points
+		}
+	    else if (rate == CAN_250K)       //!< -- 250Kb/s, 16x Tscl, sampling at 75%
+	    {
+	        CONF_CANBT1 = 0x02;       // Tscl  = 2x Tclkio = 250 ns
+	        CONF_CANBT2 = 0x0C;       // Tsync = 1x Tscl, Tprs = 7x Tscl, Tsjw = 1x Tscl
+	        CONF_CANBT3 = 0x37;       // Tpsh1 = 4x Tscl, Tpsh2 = 4x Tscl, 3 sample points
+		}
+	    else if (rate == CAN_500K)       //!< -- 500Kb/s, 8x Tscl, sampling at 75%
+	    {
+	        CONF_CANBT1 = 0x02;       // Tscl  = 2x Tclkio = 250 ns
+	        CONF_CANBT2 = 0x04;       // Tsync = 1x Tscl, Tprs = 3x Tscl, Tsjw = 1x Tscl
+	        CONF_CANBT3 = 0x13;       // Tpsh1 = 2x Tscl, Tpsh2 = 2x Tscl, 3 sample points
+		}
+	    else if (rate == CAN_1000K)      //!< -- 1 Mb/s, 8x Tscl, sampling at 75%
+	    {
+	        CONF_CANBT1 = 0x00;       // Tscl  = 1x Tclkio = 125 ns
+	        CONF_CANBT2 = 0x04;       // Tsync = 1x Tscl, Tprs = 3x Tscl, Tsjw = 1x Tscl
+	        CONF_CANBT3 = 0x13;       // Tpsh1 = 2x Tscl, Tpsh2 = 2x Tscl, 3 sample points
+		}
+	#else
+	#   error The CLK_SPEED_IN_MHZ is not set
+	#endif
+	
+	Can_conf_bt()	//configure fixed baud rate by assigning CANBT registers to the values above
+	
+	Can_reset()
+	can_clear_all_mob();
+	Can_enable();
+	
+	if (mode == CAN_LISTEN)
+	{
+		CANGCON |= (1<<LISTEN)  //turn the LISTEN bit (Bit 3) of CANGCON on
+	}
+	else if (mode = CAN_NORMAL)
+	{
+		CANGCON &= !(1<<LISTEN)	//turn the LISTEN bit (Bit 3) of CANGCON off
+	}
+	
+	
+	
+	
 	
 }
 
-bool Can_object::get_multimode(void)
-{
-	//AVR's MOb's are two way
-	return true
-}
-
-bool Can_object::set_mode(Can_object_mode mode)
-{
-	if (mode == CAN_OBJ_TX)
-	{
-		Can_config_tx();	
-	}
-	else if (mode == CAN_OBJ_RX)
-	{
-		Can_config_rx();
-	}
-}
-
-Can_object_status Can_object::get_status(void)
-{
-	//TODO
-}
-
-
-/********************************************************************************************************************************/
-Can_tree Can::get_tree(void) 
-{
-	//TODO
-}
-
-Can_command_response Can::initialise(Can_rate rate, Can_mode mode)
-{
-	//TODO
-}
-
-void Can::enable_interrupts(void)
-{
-	//TODO
-}
-
-void Can::disable_interrupt(void)
-{
-	//TODO
-}
-
-void Can::attach_interrupt(void) 
-{
-	//TODO
-}
-
-void Can::detach_interrupt(void)
-{
-	//TODO
-}
-
-Can_status Can::get_status(void)
-{
-	//TODO
-}
-
-void Can::transmit(void)
-{
-	//TODO
-}
-
-bool Can::check_ready(Can_object obj)
-{
-	//TODO
-}
-
-Can_message Can::read_object(Can_object obj) 
-{
-	//TODO
-}
-
-Can::~Can(void)
-{
-	// Make sure we have vacated the semaphore before we go out of scope.
-	vacate();
-}
-
-void Can::vacate(void) 
-{
-	//TODO
-}
-
-
-bool done_init = false;
-void can_init(void)
-{
-	done_init = true;
-}
-
-Can Can::grab(Can_channel can_channel)
-{
-	if (!done_init) 
-	{
-		can_init();
-	}
-}
-
-Can_tree Can::get_tree(void)
-{
-	Can_tree
-}
 
