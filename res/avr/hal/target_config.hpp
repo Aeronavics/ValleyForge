d5561dfe26bbe0193ddc6a917fd07fcb35a0463c
student <gjx10@uclive.ac.nz>
2013-11-22 12:57:05 +1300

    Corrected values for AT90CAN128
    Former-commit-id: 7a65e97dc4c961f45c8079acf6e2c7f4d4ab7b43
    Former-commit-id: 2278d997ad0b62f643aa72fbbf0cf73e7c2351fd [formerly c7d029fe3ca930bb305d1f687360da991f7c6e29]
    Former-commit-id: ae15658b9f7ff8a1517f1c9f8ad35b1e75bbfc94

--- res/avr/hal/target_config.hpp
+++ res/avr/hal/target_config.hpp
@@ -215,17 +215,17 @@ static const int_bank_t PC_INT[NUM_PORTS][NUM_PINS] =  {{PCINT_NONE, PCINT_NONE,
 
 #	define T4_SIZE			0
 #	define T5_SIZE			0
-						// NOTE THESE NUMBERS (below) ARE WRONG, THEY ARE JUST COPY PASTED SO IT WILL COMPILE. IF TO BE USED THEY WILL HAVE TO BE FILLED IN!
-#	define NUM_BANKS		3	// Contains 3 PC_INT banks. Only one pin on each bank can be used at a time.
+
+#	define NUM_BANKS		0	// Contains 3 PC_INT banks. Only one pin on each bank can be used at a time.
 #	define EXT_INT_SIZE		8
 #	define INT_DIFF_OFFSET		3
 
 #	define EXTERNAL_NUM_INTERRUPTS	NUM_BANKS + EXT_INT_SIZE
 							      
-#	define PC_INT_SIZE		24
+#	define PC_INT_SIZE		0
 
 #	define NUM_8_BIT_TIMERS		2
-#	define NUM_16_BIT_TIMERS	1
+#	define NUM_16_BIT_TIMERS	2
 # 	define NUM_TIMERS		NUM_8_BIT_TIMERS + NUM_16_BIT_TIMERS
 
 #	define INT_BIT			7
