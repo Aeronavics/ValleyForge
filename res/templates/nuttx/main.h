b029d50c0ccccd190b6919efedf6a0dfa4307d9a
Zane Barker <zgb10@uclive.ac.nz>
2014-12-02 10:47:13 +1300

    Renamed the NuttX .c template file and added a .h template file.
    Former-commit-id: 807386d6ea70f1b7301b826cf5a93e489b945c3f [formerly 8eaadd0095e6a370f23066b369b1b0e42dc63da7] [formerly c3354508f8cf7762514531872a2f3d63597bdf3e [formerly cf1ee8bb697c5f20674ee83e83c164dbc1f07047]]
    Former-commit-id: 5dbee254156950c80420dd614bbc0d3b07bb8ece [formerly 0e96e8d34e9f34064eb1490f507f994f973b03bd]
    Former-commit-id: 8383fb91ed1126f8055ba8e2437eb39d63b86cff

---
+++ res/templates/nuttx/main.h
@@ -0,0 +1,70 @@
+/********************************************************************************************************************************
+ *
+ *  FILE:               <<<TC_INSERTS_FILE_NAME_HERE>>>
+ *
+ *  SUB-SYSTEM:         <<<TC_INSERTS_SUBSYSTEM_HERE>>>
+ *
+ *  COMPONENT:          <<<TC_INSERTS_COMPONENT_NAME_HERE>>>
+ *
+ *  TARGET:             <<<TC_INSERTS_TARGET_HERE>>>
+ *
+ *  PLATFORM:           <<<TC_INSERTS_PLATFORM_HERE>>>
+ *
+ *  AUTHOR:             <<<TC_INSERTS_AUTHOR_NAME_HERE>>>
+ *
+ *  DATE CREATED:       <<<TC_INSERTS_DATE_HERE>>>
+ *
+ *	This is the header file which matches <<<TC_INSERTS_C_FILE_NAME_HERE>>>...
+ * 
+ ********************************************************************************************************************************/
+
+// Support linking this C library from C++.
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Only include this header file once.
+#ifndef __<<<TC_INSERTS_UC_FILE_BASENAME_HERE>>>_H__
+#define __<<<TC_INSERTS_UC_FILE_BASENAME_HERE>>>_H__
+
+// INCLUDE REQUIRED HEADER FILES FOR INTERFACE.
+
+// Include the required IO header file.
+#include >>>TC_INSERTS_IO_FILE_NAME_HERE<<<
+
+// Include the STDINT fixed width types.
+#include >>>TC_INSERTS_STDINT_FILE_NAME_HERE<<<
+
+// Include the C99 boolean type.
+#include <stdbool.h>
+
+// DEFINE PUBLIC PREPROCESSOR MACROS.
+
+// DEFINE PUBLIC TYPES AND ENUMERATIONS.
+
+// DECLARE PUBLIC GLOBAL VARIABLES.
+
+// FORWARD DEFINE PRIVATE PROTOTYPES.
+
+// DEFINE PUBLIC FUNCTION PROTOTYPES.
+
+/**
+ * Description of what the function does...
+ *
+ * @param	Details of arguments to the function.
+ * @return	Details of return value from the function.
+ */
+#ifdef CONFIG_BUILD_TERMINAL
+int main(int argc, FAR char *argv[]);
+#else
+int <<<TC_INSERTS_COMPONENT_NAME_HERE>>>_main(int argc, char *argv[])
+#endif
+
+#endif // __<<<TC_INSERTS_UC_FILE_BASENAME_HERE>>>_H__
+
+// Support linking this C library from C++.
+#ifdef __cplusplus
+}
+#endif
+
+// ALL DONE.
