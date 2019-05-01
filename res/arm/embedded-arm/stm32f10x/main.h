d0a0d9875b06d360ceaa0be14b3970c18ef6feb7
Anton Slooten <anton@aeronavics.com>
2019-05-01 17:10:47 +1200

    C to c++ linking

--- res/arm/embedded-arm/stm32f10x/main.h
+++ res/arm/embedded-arm/stm32f10x/main.h
@@ -3,6 +3,11 @@
 //This is a god awful hack. The stm32cube generates all it's unclude that are designed to include the main file !?
 //Heirachy guys.
 
+#ifndef __main_H
+#define __main_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
 //As we do not use main.h, this is soly included to provide the required includes.
 #include "stm32f1xx_hal.h"
 #include "stm32f1xx_hal_rcc.h"
@@ -10,3 +15,8 @@
 
 void SystemClock_Config(void);
 void Error_Handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ main_H */
