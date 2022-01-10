37c47999839764dcf14bc6cad5f504f88c231eb9
Anton Slooten <anton@aeronavics.com>
2022-01-11 09:35:07 +1300

    Adding reset cause to the stm

--- res/arm/embedded-arm/stm32f10x/main.h
+++ res/arm/embedded-arm/stm32f10x/main.h
@@ -16,8 +16,25 @@
     #include "stm32f1xx_ll_gpio.h"
     #include "stm32f1xx_ll_bus.h"
 #endif
+
+/// @brief  Possible STM32 system reset causes
+typedef enum reset_cause_e
+{
+    RESET_CAUSE_UNKNOWN = 0,
+    RESET_CAUSE_LOW_POWER_RESET,
+    RESET_CAUSE_WINDOW_WATCHDOG_RESET,
+    RESET_CAUSE_INDEPENDENT_WATCHDOG_RESET,
+    RESET_CAUSE_SOFTWARE_RESET,
+    RESET_CAUSE_POWER_ON_POWER_DOWN_RESET,
+    RESET_CAUSE_EXTERNAL_RESET_PIN_RESET,
+    RESET_CAUSE_BROWNOUT_RESET,
+} reset_cause_t;
+
 void SystemClock_Config(void);
 void Error_Handler(void);
+reset_cause_t reset_cause_get(void);
+const char * reset_cause_get_name(reset_cause_t reset_cause);
+
 
 #ifdef __cplusplus
 }
